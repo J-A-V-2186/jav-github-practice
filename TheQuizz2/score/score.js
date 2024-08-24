ae83343bde93d985117086e26c4d1f0d65e08fe7
J-A-V-2186 <jesusav2186@gmail.com>
2024-08-24 15:15:57 -0400

    Implemented a scoring system with ternary operations.

--- TheQuizz2/score/score.js
+++ TheQuizz2/score/score.js
@@ -1,13 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const content = document.querySelector('main')
-    console.log(content);
-    setTimeout(() => {
-        content.classList.add('visible');
-    }, 100);
+document.addEventListener("DOMContentLoaded", () => {
+  const content = document.querySelector("main");
+  console.log(content);
+  setTimeout(() => {
+    content.classList.add("visible");
+  }, 100);
 });
 const score = localStorage.getItem("quizScore");
-const gradeDiv = document.querySelector('.grade')
+const gradeDiv = document.querySelector(".grade");
+const gradePercentage = document.querySelector(".grade__percentage");
+const gradeLetter = document.querySelector(".grade__leter");
 console.log(score);
-gradeDiv.textContent = `${score} `
+gradeDiv.textContent = `${score}/10`;
+gradePercentage.textContent = `${(score / 10) * 100}%`;
 
-     
+score === 10 ? (gradeLetter.textContent = "A") : null;
+score === 8 ? (gradeLetter.textContent = "B") : null;
+score === 7 ? (gradeLetter.textContent = "C") : null;
+score === 6 ? (gradeLetter.textContent = "D") : null;
+score <= 5 ? (gradeLetter.textContent = "E") : null;
